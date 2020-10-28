3c97bc52603c75a4e032e8255170a851746595c5
mijaplex <mijail.febres@gmail.com>
2020-10-28 13:48:46 +0100

    Uses gradients at the nodes. Distance function is better for unstructured grids.

--- Sources/Process/Multiphase_Mod/Vof_Solver_Dist_Function_Cell_Loop.f90
+++ Sources/Process/Multiphase_Mod/Vof_Solver_Dist_Function_Cell_Loop.f90
@@ -1,137 +1,112 @@
 !==============================================================================!
-  subroutine Multiphase_Mod_Vof_Solver_Dist_Function_Cell_Loop(grid,        &
+  subroutine Multiphase_Mod_Vof_Solver_Dist_Function_Cell_Loop(flow,        &
                                                                a,           &
                                                                dist_curr,   &
                                                                r_phi,       &
-                                                               s_0,         &
                                                                grad_i,      &
                                                                grad_j,      &
                                                                grad_k)
 !------------------------------------------------------------------------------!
 !    Loop on cells for calculation of the Distance function. This function     !
 !    only reduces the size of Compute_Distance_Function
+!----------------------------------[Modules]-----------------------------------!
+  use Work_Mod, only: gra_x  => r_node_05,  &
+                      gra_y  => r_node_06,  &
+                      gra_z  => r_node_07,  &
+                      dist_n => r_node_08
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type)           :: grid
+  type(Field_Type),  target :: flow
   type(Matrix_Type), target :: a
-  real                      :: grad_i   (-grid % n_bnd_cells : grid % n_cells),&
-                               grad_j   (-grid % n_bnd_cells : grid % n_cells),&
-                               grad_k   (-grid % n_bnd_cells : grid % n_cells),&
-                               dist_curr(-grid % n_bnd_cells : grid % n_cells),&
-                               r_phi    (-grid % n_bnd_cells : grid % n_cells),&
-                               s_0      (-grid % n_bnd_cells : grid % n_cells)
+  real                      :: grad_i   (-flow % pnt_grid % n_bnd_cells    &
+                                        : flow % pnt_grid % n_cells),      &
+                               grad_j   (-flow % pnt_grid % n_bnd_cells    &
+                                        : flow % pnt_grid % n_cells),      &
+                               grad_k   (-flow % pnt_grid % n_bnd_cells    &
+                                        : flow % pnt_grid % n_cells),      &
+                               dist_curr(-flow % pnt_grid % n_bnd_cells    &
+                                        : flow % pnt_grid % n_cells),      &
+                               r_phi    (-flow % pnt_grid % n_bnd_cells    &
+                                        : flow % pnt_grid % n_cells)
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: c, s, c1, c2
-  integer :: i_fac
+  type(Grid_Type),  pointer :: grid
+  integer :: c, s, c1, c2, nb, nc, nn
+  integer :: i_fac, i_nod, n
   real    :: fs
-  real    :: dot_p1, dot_p2, grad_fn
+  real    :: dot_p1, dot_p2, grad_fn(3)
   real    :: g_fx, g_fy, g_fz
-  real    :: a_i(6), b_i(6), c_i(6), r_fac(3)
+  real    :: a_i(8), b_i(8), c_i(8), r_nod(3)
   real    :: corr_x, corr_y, corr_z
 !==============================================================================!
+
+  grid => flow % pnt_grid
+
+  nb = grid % n_bnd_cells
+  nc = grid % n_cells
+  nn = grid % n_nodes
+
+  ! Interpolate to nodes
+
+  call Field_Mod_Interpolate_Cells_To_Nodes(flow,                     &
+                                      dist_curr(-nb:nc), dist_n(1:nn))
+
+  ! Find gradient at nodal positions
+  call Field_Mod_Grad_Component_Cells_To_Nodes(flow,                     &
+                                               dist_curr, dist_n,  &
+                                            1, gra_x(1:nn))
+  call Field_Mod_Grad_Component_Cells_To_Nodes(flow,                     &
+                                               dist_curr, dist_n,  &
+                                            2, gra_y(1:nn))
+  call Field_Mod_Grad_Component_Cells_To_Nodes(flow,                     &
+                                               dist_curr, dist_n,  &
+                                            3, gra_z(1:nn))
 
   do c = 1, grid % n_cells
 
     a_i = -HUGE; b_i = - HUGE; c_i = -HUGE
 
-    ! Loop on faces
-    do i_fac = 1, grid % cells_n_faces(c)
+    ! Loop on nodes
+    do i_nod = 1, grid % cells_n_nodes(c)
+      n  = grid % cells_n(i_nod, c)
+      r_nod = (/grid % xn(n) - grid % xc(c),   &
+                grid % yn(n) - grid % yc(c),   &
+                grid % zn(n) - grid % zc(c)/)
 
-      s = grid % cells_f(i_fac, c)
-      c1 = grid % faces_c(1,s)
-      c2 = grid % faces_c(2,s)
-      fs = grid % f(s)
-
-      corr_x = 0.0; corr_y = 0.0; corr_z = 0.0
-
-      if (c1 .eq. c) then
-        if (c2 > 0) then
-          g_fx = fs * grad_i(c1) + (1.0 - fs) * grad_i(c2)
-          g_fy = fs * grad_j(c1) + (1.0 - fs) * grad_j(c2)
-          g_fz = fs * grad_k(c1) + (1.0 - fs) * grad_k(c2)
-        else
-          g_fx = grad_i(c1)
-          g_fy = grad_j(c1)
-          g_fz = grad_k(c1)
-        end if
-
-        dot_p1 = dot_product((/g_fx, g_fy, g_fz/),                         &
-                             (/grid % sx(s), grid % sy(s), grid % sz(s)/))
-
-        dot_p2 = dot_product((/g_fx, g_fy, g_fz/),                         &
-                             (/grid % dx(s), grid % dy(s), grid % dz(s)/))
-
-        grad_fn = dot_p1 + a % fc(s) * ( dist_curr(c2) - dist_curr(c1)   &
-                                       - dot_p2 )
-
-        r_fac = (/grid % xf(s) - grid % xc(c),   &
-                  grid % yf(s) - grid % yc(c),   &
-                  grid % zf(s) - grid % zc(c)/)
+      if( (dist_curr(c) > 0.0 .and. r_nod(1) > 0.0) .or.     &
+          (dist_curr(c) < 0.0 .and. r_nod(1) < 0.0) ) then
+        a_i(i_nod) = min(0.0, gra_x(n)) ** 2
       else
-        ! Correction for periodic faces:
-        call Grid_Mod_Correction_Periodicity(grid, s,   &
-                                             corr_x, corr_y, corr_z)
-
-        g_fx = fs * grad_i(c1) + (1.0 - fs) * grad_i(c2)
-        g_fy = fs * grad_j(c1) + (1.0 - fs) * grad_j(c2)
-        g_fz = fs * grad_k(c1) + (1.0 - fs) * grad_k(c2)
-
-        dot_p1 = -dot_product((/g_fx, g_fy, g_fz/),                        &
-                             (/grid % sx(s), grid % sy(s), grid % sz(s)/))
-
-        dot_p2 = -dot_product((/g_fx, g_fy, g_fz/),                        &
-                             (/grid % dx(s), grid % dy(s), grid % dz(s)/))
-
-
-        grad_fn = dot_p1 + a % fc(s) * ( dist_curr(c1) - dist_curr(c2)   &
-                                       - dot_p2 )
-
-        r_fac = -(/( grid % xf(s) + corr_x ) - grid % xc(c),   &
-                   ( grid % yf(s) + corr_y ) - grid % yc(c),   &
-                   ( grid % zf(s) + corr_z ) - grid % zc(c)/)
-
-      end if
-
-      grad_fn = grad_fn / grid % s(s)
-
-      if( (dist_curr(c) > 0.0 .and. r_fac(1) > 0.0) .or.     &
-          (dist_curr(c) < 0.0 .and. r_fac(1) < 0.0) ) then
-        a_i(i_fac) = min(0.0, grad_fn * grid % sx(s) / grid % s(s)) ** 2
-      else
-        if( (dist_curr(c) > 0.0 .and. r_fac(1) < 0.0) .or.     &
-            (dist_curr(c) < 0.0 .and. r_fac(1) > 0.0) ) then
-          a_i(i_fac) = max(0.0, grad_fn * grid % sx(s) / grid % s(s)) ** 2
+        if( (dist_curr(c) > 0.0 .and. r_nod(1) < 0.0) .or.     &
+            (dist_curr(c) < 0.0 .and. r_nod(1) > 0.0) ) then
+          a_i(i_nod) = max(0.0, gra_x(n)) ** 2
         end if
       end if
 
-      if( (dist_curr(c) > 0.0 .and. r_fac(2) > 0.0) .or.     &
-          (dist_curr(c) < 0.0 .and. r_fac(2) < 0.0) ) then
-        b_i(i_fac) = min(0.0, grad_fn * grid % sy(s) / grid % s(s)) ** 2
+      if( (dist_curr(c) > 0.0 .and. r_nod(2) > 0.0) .or.     &
+          (dist_curr(c) < 0.0 .and. r_nod(2) < 0.0) ) then
+        b_i(i_nod) = min(0.0, gra_y(n)) ** 2
       else
-        if( (dist_curr(c) > 0.0 .and. r_fac(2) < 0.0) .or.     &
-            (dist_curr(c) < 0.0 .and. r_fac(2) > 0.0) ) then
-          b_i(i_fac) = max(0.0, grad_fn * grid % sy(s) / grid % s(s)) ** 2
+        if( (dist_curr(c) > 0.0 .and. r_nod(2) < 0.0) .or.     &
+            (dist_curr(c) < 0.0 .and. r_nod(2) > 0.0) ) then
+          b_i(i_nod) = max(0.0, gra_y(n)) ** 2
         end if
       end if
 
-      if( (dist_curr(c) > 0.0 .and. r_fac(3) > 0.0) .or.     &
-          (dist_curr(c) < 0.0 .and. r_fac(3) < 0.0) ) then
-        c_i(i_fac) = min(0.0, grad_fn * grid % sz(s) / grid % s(s)) ** 2
+      if( (dist_curr(c) > 0.0 .and. r_nod(3) > 0.0) .or.     &
+          (dist_curr(c) < 0.0 .and. r_nod(3) < 0.0) ) then
+        c_i(i_nod) = min(0.0, gra_z(n)) ** 2
       else
-        if( (dist_curr(c) > 0.0 .and. r_fac(3) < 0.0) .or.     &
-            (dist_curr(c) < 0.0 .and. r_fac(3) > 0.0) ) then
-          c_i(i_fac) = max(0.0, grad_fn * grid % sz(s) / grid % s(s)) ** 2
+        if( (dist_curr(c) > 0.0 .and. r_nod(3) < 0.0) .or.     &
+            (dist_curr(c) < 0.0 .and. r_nod(3) > 0.0) ) then
+          c_i(i_nod) = max(0.0, gra_z(n)) ** 2
         end if
       end if
+    end do ! End loop nodes
 
-    end do  ! end loop faces
-
-    r_phi(c) = s_0(c) * ( 1.0                                    &
-             - sqrt( maxval(a_i(1:grid % cells_n_faces(c)))      &
-                   + maxval(b_i(1:grid % cells_n_faces(c)))      &
-                   + maxval(c_i(1:grid % cells_n_faces(c))) ) )
-
-  end do  ! end loop cells
+    r_phi(c) = sqrt( maxval(a_i(1:grid % cells_n_nodes(c)))      &
+                   + maxval(b_i(1:grid % cells_n_nodes(c)))      &
+                   + maxval(c_i(1:grid % cells_n_nodes(c))) )
+  end do
 
   end subroutine
