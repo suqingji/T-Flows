f82506f97b7da33c55eac0f8816f509adb8a547b
mijaplex <mijail.febres@gmail.com>
2020-10-28 13:53:06 +0100

    Two things: 1. We can chose if smoothing is done with VOF or Heaviside funct. 2. Curvature expands to two additional layers of cells.

--- Sources/Process/Multiphase_Mod/Vof_Smooth_Curvature.f90
+++ Sources/Process/Multiphase_Mod/Vof_Smooth_Curvature.f90
@@ -8,11 +8,9 @@
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Work_Mod, only: k_star       => r_cell_14,    &
-                      gradk_x      => r_cell_15,    &
-                      gradk_y      => r_cell_16,    &
-                      gradk_z      => r_cell_17,    &
                       sum_k_weight => r_cell_18,    &
-                      sum_weight   => r_cell_19
+                      sum_weight   => r_cell_19,    &
+                      wint         => i_cell_02
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
@@ -22,19 +20,19 @@
   real                          :: norm_ny(-grid % n_bnd_cells:grid % n_cells)
   real                          :: norm_nz(-grid % n_bnd_cells:grid % n_cells)
 !-----------------------------------[Locals]-----------------------------------!
-  type(Var_Type), pointer :: vof
-  integer                 :: s, c, c1, c2, c_iter, i_fac, nb, nc
+  real          , pointer :: vof(:)
+  integer                 :: i, s, c, c1, c2, c_iter, iter, i_fac, nb, nc
   integer                 :: face_init, face_end, face_step
   real                    :: fs, w_v1, w_v2, w_m1, w_m2
-  real                    :: weight_s, weight_n
+  real                    :: weight_s, weight_n, sum1, sum2
   real                    :: norma, epsloc, curvf, dotprod
 !==============================================================================!
 
   ! Take aliases
   if (mult % d_func) then
-    vof => mult % dist_func
+    vof => mult % dist_func % oo
   else
-    vof => mult % vof
+    vof => mult % vof % n
   end if
 
   nb = grid % n_bnd_cells
@@ -55,38 +53,12 @@
   ! Preliminary results using wetting (with symmetry in some boundaries) show
   ! it is better not to take into aacount the boundaries
 
-  !  gradient of curvature, only interior
-  gradk_x(-nb:nc) = 0.0
-  gradk_y(-nb:nc) = 0.0
-  gradk_z(-nb:nc) = 0.0
-
-  do s = grid % n_bnd_faces + 1, grid % n_faces
-    c1 = grid % faces_c(1,s)
-    c2 = grid % faces_c(2,s)
-    fs = grid % f(s)
-
-    curvf = fs * mult % curv(c1) + (1.0 - fs) * mult % curv(c2)
-
-    gradk_x(c1) = gradk_x(c1) + curvf * grid % sx(s) / grid % vol(c1)
-    gradk_y(c1) = gradk_y(c1) + curvf * grid % sy(s) / grid % vol(c1)
-    gradk_z(c1) = gradk_z(c1) + curvf * grid % sz(s) / grid % vol(c1)
-
-    gradk_x(c2) = gradk_x(c2) - curvf * grid % sx(s) / grid % vol(c2)
-    gradk_y(c2) = gradk_y(c2) - curvf * grid % sy(s) / grid % vol(c2)
-    gradk_z(c2) = gradk_z(c2) - curvf * grid % sz(s) / grid % vol(c2)
-
-  end do
-
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_x(-nb:nc))
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_y(-nb:nc))
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_z(-nb:nc))
-
   ! Interior faces
   do s = grid % n_bnd_faces + 1, grid % n_faces
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
-    w_v1 = (1.0 - 2.0 * abs(0.5 - vof % n(c1))) ** weight_s
-    w_v2 = (1.0 - 2.0 * abs(0.5 - vof % n(c2))) ** weight_s
+    w_v1 = (1.0 - 2.0 * abs(0.5 - vof(c1))) ** weight_s
+    w_v2 = (1.0 - 2.0 * abs(0.5 - vof(c2))) ** weight_s
     sum_k_weight(c1) = sum_k_weight(c1) + mult % curv(c2) * w_v2
     sum_weight(c1) = sum_weight(c1) + w_v2
 
@@ -98,7 +70,7 @@
   call Grid_Mod_Exchange_Cells_Real(grid, sum_weight  (-nb:nc))
 
   do c = 1, grid % n_cells
-    w_v1 = (1.0 - 2.0 * abs(0.5 - vof % n(c))) ** weight_s
+    w_v1 = (1.0 - 2.0 * abs(0.5 - vof(c))) ** weight_s
     k_star(c) = (w_v1 * mult % curv(c) + sum_k_weight(c))    &
               / (w_v1 + sum_weight(c) + epsloc)
   end do
@@ -112,38 +84,12 @@
   sum_k_weight(-nb:nc) = 0.0
   sum_weight  (-nb:nc) = 0.0
 
-  !  gradient of curvature, only interior
-  gradk_x(-nb:nc) = 0.0
-  gradk_y(-nb:nc) = 0.0
-  gradk_z(-nb:nc) = 0.0
-
-  do s = grid % n_bnd_faces + 1, grid % n_faces
-    c1 = grid % faces_c(1,s)
-    c2 = grid % faces_c(2,s)
-    fs = grid % f(s)
-
-    curvf = fs * k_star(c1) + (1.0 - fs) * k_star(c2)
-
-    gradk_x(c1) = gradk_x(c1) + curvf * grid % sx(s) / grid % vol(c1)
-    gradk_y(c1) = gradk_y(c1) + curvf * grid % sy(s) / grid % vol(c1)
-    gradk_z(c1) = gradk_z(c1) + curvf * grid % sz(s) / grid % vol(c1)
-
-    gradk_x(c2) = gradk_x(c2) - curvf * grid % sx(s) / grid % vol(c2)
-    gradk_y(c2) = gradk_y(c2) - curvf * grid % sy(s) / grid % vol(c2)
-    gradk_z(c2) = gradk_z(c2) - curvf * grid % sz(s) / grid % vol(c2)
-
-  end do
-
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_x(-nb:nc))
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_y(-nb:nc))
-  call Grid_Mod_Exchange_Cells_Real(grid, gradk_z(-nb:nc))
-
   ! Interior faces
   do s = grid % n_bnd_faces + 1, grid % n_faces
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
-    w_v1 = (1.0 - 2.0 * abs(0.5 - vof % n(c1))) ** weight_n
-    w_v2 = (1.0 - 2.0 * abs(0.5 - vof % n(c2))) ** weight_n
+    w_v1 = (1.0 - 2.0 * abs(0.5 - vof(c1))) ** weight_n
+    w_v2 = (1.0 - 2.0 * abs(0.5 - vof(c2))) ** weight_n
 
     w_m1 = abs(dot_product((/norm_nx(c1), norm_ny(c1), norm_nz(c1)/),     &
                            (/grid % dx(s), grid % dy(s), grid % dz(s)/)   &
@@ -164,11 +110,68 @@
   call Grid_Mod_Exchange_Cells_Real(grid, sum_weight  (-nb:nc))
 
   do c = 1, grid % n_cells
-    w_v1 = (1.0 - 2.0 * abs(0.5 - vof % n(c))) ** weight_n
+    w_v1 = (1.0 - 2.0 * abs(0.5 - vof(c))) ** weight_n
     mult % curv(c) = (w_v1 * k_star(c) + sum_k_weight(c))    &
                    / (w_v1 + sum_weight(c) + epsloc)
   end do
 
+  !--------------------------------------------------------!
+  !   Expand curvature to two additional layers of cells   !
+  !--------------------------------------------------------!
   call Grid_Mod_Exchange_Cells_Real(grid, mult % curv)
+
+  wint = 0
+  do c = 1, grid % n_cells
+    if(vof(c) > 0.0 .and. vof(c) < 1.0) then
+      wint(c) = 1
+    end if
+  end do
+
+  call Grid_Mod_Exchange_Cells_Int(grid, wint)
+
+  iter = 2
+  do i = 1, iter
+    do c = 1, grid % n_cells
+      if (wint(c) .ne. i) then
+        sum1 = 0.0
+        sum2 = 0.0
+        do i_fac = 1, grid % cells_n_faces(c)
+          s  =  grid % cells_f(i_fac, c)
+          c1 = grid % faces_c(1,s)
+          c2 = grid % faces_c(2,s)
+          if (all((/c1,c2/) > 0)) then
+            if (c .eq. c1) then
+              if (wint(c2) == i) then
+                w_m2 = abs(dot_product((/norm_nx(c2), norm_ny(c2), norm_nz(c2)/),     &
+                                       (/grid % dx(s), grid % dy(s), grid % dz(s)/)   &
+                                       / (-grid % d(s)))) ** weight_n
+
+                sum1 = sum1 + mult % curv(c2) * w_m2
+                sum2 = sum2 + w_m2
+              end if
+            else
+              if (wint(c1) == i) then
+                w_m1 = abs(dot_product((/norm_nx(c1), norm_ny(c1), norm_nz(c1)/),     &
+                                       (/grid % dx(s), grid % dy(s), grid % dz(s)/)   &
+                                       / grid % d(s))) ** weight_n
+
+                sum1 = sum1 + mult % curv(c1) * w_m1
+                sum2 = sum2 + w_m1
+              end if
+            end if
+          end if
+        end do
+        if ((sum2 > epsloc) .and. wint(c) < i) then
+          mult % curv(c) = sum1 / sum2
+          wint(c) = i + 1
+        else if(wint(c) == 0) then
+          mult % curv(c) = 0.0
+        end if
+      end if
+    end do
+
+    call Grid_Mod_Exchange_Cells_Int(grid, wint)
+    call Grid_Mod_Exchange_Cells_Real(grid, mult % curv)
+  end do
 
   end subroutine
