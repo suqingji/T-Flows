5bba7c9e857dfa5b9b8143d8d7e024af129a8ba0
mhadziabdic <muhamed.hadziabdic@gmail.com>
2022-06-10 18:56:25 +0200

    Wall function is implemented in more elegant way that includes roughness.

--- Sources/Process/Turb_Mod/U_Plus_Log_Law.f90
+++ Sources/Process/Turb_Mod/U_Plus_Log_Law.f90
@@ -1,17 +1,25 @@
 !==============================================================================!
-  real function U_Plus_Log_Law(turb, y_plus)
+  real function U_Plus_Log_Law(turb, wall_dist, y_plus, z_o)
 !------------------------------------------------------------------------------!
-!   Calculates U+ from log law.                                                !
+!   Calculates U+ by using log law. 
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
-  use Turb_Mod, only: Turb_Type, e_log, kappa
+  use Const_Mod, only: TINY
+  use Turb_Mod,  only: Turb_Type, kappa, e_log
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   type(Turb_Type) :: turb
-  real            :: y_plus
+  real            :: y_plus, wall_dist, z_o
 !==============================================================================!
 
-  U_Plus_Log_Law = log( max(y_plus, 1.05) * e_log ) / kappa
+  if(z_o > tiny) then 
+
+    U_Plus_Log_Law = log( (wall_dist + z_o) / z_o)  &
+                      / (kappa + TINY) + TINY
+  else
+
+    U_Plus_Log_Law = log( max(y_plus, 1.05) * e_log ) / kappa
+  end if
 
   end function
